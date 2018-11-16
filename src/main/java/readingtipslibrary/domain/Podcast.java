98b181a367adddbf07dfa5c4d3c5a955dfb9988d
Susanna Rajamäki <strajama@gmail.com>
2018-11-16 09:27:01 +0200

    podcast

---
+++ src/main/java/readingtipslibrary/domain/Podcast.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package readingtipslibrary.domain;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author strajama
+ */
+public class Podcast implements Tip {
+
+    private String author;
+    private String podcastName;
+    private String title;
+    private String description;
+    private String type;
+    private ArrayList<String> tags;
+    private ArrayList<String> relatedCourses;
+
+    public Podcast(String podcastName, String title, String type) {
+        this.podcastName = podcastName;
+        this.title = title;
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+}
+/*
+Author: Sami Honkonen
+Podcastin nimi: Boss Level Podcast
+Otsikko: Jim Benson on Personal Kanban, Lean Coffee and collaboration
+Kuvaus: "Personal Kanban, which is an approach to dealing with the overload of stuff you need to deal with. 
+  We dig into into its two simple rules, visualizing work and limiting work in progress. 
+  We then walk through Lean Coffee, which is a simple and effective way to run your meetings."
+Tyyppi: Podcast
+Tagit: Kanban, Lean Coffee
+Related courses: TKT20006 Ohjelmistotuotanto
+*/
