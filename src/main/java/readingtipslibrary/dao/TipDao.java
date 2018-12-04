2f4538fcceb53fd74dece57a0d38923d55596d72
Susanna Rajamäki <strajama@gmail.com>
2018-12-04 13:13:11 +0200

    käyttäjä ei pysty tekemään sql-injektiota ja findByTitle aloitettu

--- src/main/java/readingtipslibrary/dao/TipDao.java
+++ src/main/java/readingtipslibrary/dao/TipDao.java
@@ -28,16 +28,34 @@ public class TipDao {
     public List<Tip> findAll(String type) throws SQLException {
 
         Tip t;
+        String table;
+        switch (type) {
+            case "book":
+                table = "books";
+                break;
+            case "video":
+                table = "videos";
+                break;
+            case "podcast":
+                table = "podcasts";
+                break;
+            case "blogpost":
+                table = "blogposts";
+                break;
+            default:
+                throw new SQLException("unexpected value provided for table name");
+        }
+
         Connection connection = database.getConnection();
-        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM " + type + "s");
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM " + table);// + type + "s");
 
         List<Tip> tips = new ArrayList<>();
 
         ResultSet rs = stmt.executeQuery();
-        
+
         while (rs.next()) {
             t = Tip.tipFromType(type);
-            for (String s : t.getFieldNames()){
+            for (String s : t.getFieldNames()) {
                 t.getField(s).setContent(rs.getString(s));
             }
             tips.add(t);
@@ -51,26 +69,48 @@ public class TipDao {
     }
 
     public Tip insert(Tip t) throws SQLException {
+
+        String table;
+        switch (t.getType().getContent()) {
+            case "book":
+                table = "books";
+                break;
+            case "video":
+                table = "videos";
+                break;
+            case "podcast":
+                table = "podcasts";
+                break;
+            case "blogpost":
+                table = "blogposts";
+                break;
+            default:
+                throw new SQLException("unexpected value provided for table name");
+        }
+
         Connection connection = database.getConnection();
-        String statementString = "INSERT INTO " + t.getType().getContent() + "s (";
+        String statementString = "INSERT INTO " + table + "(";
+//        String statementString = "INSERT INTO " + t.getType().getContent() + "s (";
         String[] fieldNames = t.getFieldNames();
-        for( int i = 0; i < fieldNames.length; ++i){
-           if (i != 0)
-               statementString += ", ";
+        for (int i = 0; i < fieldNames.length; ++i) {
+            if (i != 0) {
+                statementString += ", ";
+            }
             statementString += fieldNames[i];
         }
         statementString += ") VALUES (";
-        for( int i = 0; i < fieldNames.length; ++i){
-           if (i != 0)
-               statementString += ", ";
+        for (int i = 0; i < fieldNames.length; ++i) {
+            if (i != 0) {
+                statementString += ", ";
+            }
             statementString += "?";
         }
         statementString += ")";
         PreparedStatement stmt = connection.prepareStatement(statementString);
-        for( int i = 0; i < fieldNames.length; ++i){
-            stmt.setObject(i+1, t.getField(fieldNames[i]).getContent());
+        for (int i = 0; i < fieldNames.length; ++i) {
+            stmt.setObject(i + 1, t.getField(fieldNames[i]).getContent());
         }
-        
+
         stmt.executeUpdate();
         stmt.close();
         connection.close();
@@ -78,8 +118,55 @@ public class TipDao {
     }
 
     public void deleteAll(String type) throws SQLException {
+
+        String table;
+        switch (type) {
+            case "book":
+                table = "books";
+                break;
+            case "video":
+                table = "videos";
+                break;
+            case "podcast":
+                table = "podcasts";
+                break;
+            case "blogpost":
+                table = "blogposts";
+                break;
+            default:
+                throw new SQLException("unexpected value provided for table name");
+        }
+
         Connection connection = database.getConnection();
-        PreparedStatement stmt = connection.prepareStatement("DELETE FROM " + type + "s"); 
+        PreparedStatement stmt = connection.prepareStatement("DELETE FROM " + table);
+        stmt.executeUpdate();
+        stmt.close();
+        connection.close();
+    }
+
+    // kesken!
+    public void findByTitle(String title, String type) throws SQLException {
+
+        String table;
+        switch (type) {
+            case "book":
+                table = "books";
+                break;
+            case "video":
+                table = "videos";
+                break;
+            case "podcast":
+                table = "podcasts";
+                break;
+            case "blogpost":
+                table = "blogposts";
+                break;
+            default:
+                throw new SQLException("unexpected value provided for table name");
+        }
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT FROM " + table + " WHERE title=?");
+        stmt.setObject(1, title);
         stmt.executeUpdate();
         stmt.close();
         connection.close();
